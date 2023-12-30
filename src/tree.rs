4b437e7d1abb44f1604a00e7cadcfb9142bdb14b
Roman Roibu <roman@roibu.xyz>
2023-12-30 17:00:36 +0100

    Implement MerkleTree.num_leaves()

--- src/tree.rs
+++ src/tree.rs
@@ -2,6 +2,7 @@ use crate::node::{Node, NodeHash};
 
 #[derive(Debug)]
 pub struct MerkleTree<H: NodeHash> {
+    depth: u32,
     root_node: Box<Node<H>>,
 }
 
@@ -9,10 +10,14 @@ impl<H: NodeHash> MerkleTree<H> {
     pub fn new(depth: u32, initial_leaf: H) -> Self {
         let root_node = Node::new(depth, initial_leaf);
         let root_node = Box::new(root_node);
-        MerkleTree { root_node }
+        MerkleTree { depth, root_node }
     }
 
     pub fn root(&self) -> &H {
         self.root_node.hash()
+    }
+
+    pub fn num_leaves(&self) -> u64 {
+        2_u64.pow(self.depth)
     }
 }
